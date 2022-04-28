92499ee0e66103692b9f5988685aa7be91369e6f
Chris Golding <chris.golding@gmail.com>
2022-04-27 21:30:59 -0400

    Initial Commit

---
+++ Animation Playground/Animation_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_PlaygroundApp.swift
+//  Animation Playground
+//
+//  Created by Chris Golding on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Animation_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation Playground
+//
+//  Created by Chris Golding on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
